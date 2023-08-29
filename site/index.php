0e342bbf6c34db2c65205e432340a14419a9b2c4
Meesterdaaf <d.vanoudheusden@rocva.nl>
2023-08-29 16:26:25 +0200

    starting files ready to go!

---
+++ site/index.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once 'header.php';
+?>
+<header>
+    <h1>
+        Welkom bij Hotel Nova
+    </h1>
+    <p>
+        Op deze site krijg je een impressie van het hotel en kun je uiteraard een kamer reserveren.
+    </p>
+</header>
+
+<?php
+include_once 'footer.php';
